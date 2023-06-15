1bfdd6cb52957ee8e24e4b016ebf571725aaf128
always_spring01 <hangbum01@gmail.com>
2023-06-15 14:10:13 +0900

    modify view - Board.hpp - Drawable.hpp - growthitem 추가

--- view/Board.hpp
+++ view/Board.hpp
@@ -1,43 +1,63 @@
 #pragma once
-#include "Drawable.hpp"
+
 #include <ncurses.h>
+#include "Drawable.hpp"
+#include <stdlib.h>
 
 class Board {
 public:
-  Board() { construct(0, 0); }
+    Board() {
+        construct(0, 0);
+    }
 
-  Board(int height, int width) { construct(height, width); }
+    Board(int height, int width) {
+        construct(height, width);
+    }
 
-  void initalize() {
-    clear();
-    refresh();
-  }
+    void initalize() {
+        clear();
+        refresh();
+    }
 
-  void addBoarder() { box(board_win, 0, 0); }
+    void addBoarder() {
+        box(board_win, 0, 0);
+    }
 
-  void add(Drawable drawable) {
-    addAt(drawable.getY(), drawable.getX(), drawable.getIcon());
-  }
+    void add(Drawable drawable) {
+        addAt(drawable.getY(), drawable.getX(), drawable.getIcon());
+    }
 
-  void addAt(int y, int x, chtype ch) { mvwaddch(board_win, y, x, ch); }
+    void addAt(int y, int x, chtype ch) {
+        mvwaddch(board_win, y, x, ch);
+    }
 
-  chtype getInput() { return wgetch(board_win); }
+    chtype getInput() {
+        return wgetch(board_win);
+    }
 
-  void clear() {
-    wclear(board_win);
-    addBoarder();
-  }
+    void getEmptyCoordinates(int &y, int &x) {
+        while((mvwinch(board_win, y = rand() % height, x = rand() % width)) != ' ');
+    }
 
-  void refresh() { wrefresh(board_win); }
+    void clear() {
+        wclear(board_win);
+        addBoarder();
+    }
+
+    void refresh() {
+        wrefresh(board_win);
+    }
 
 private:
-  WINDOW *board_win;
+    WINDOW *board_win;
+    int height, width;
 
-  void construct(int height, int width) {
-    int xMax, yMax;
-    getmaxyx(stdscr, yMax, xMax);
+    void construct(int height, int width) {
+        int xMax, yMax;
+        getmaxyx(stdscr, yMax, xMax);
+        this->height = height;
+        this->width = width;
 
-    board_win = newwin(height, width, (yMax / 2) - (height / 2),
-                       (xMax / 2) - (width / 2));
-  }
+        board_win = newwin(height, width, (yMax/2)-(height/2), (xMax/2)-(width/2));
+    }
 };
