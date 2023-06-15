c8d859413093837ecdc08532c040d0b005bac0a7
always_spring01 <hangbum01@gmail.com>
2023-06-15 12:48:46 +0900

    add view - Board.hpp - Drawable.hpp

---
+++ view/Board.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "Drawable.hpp"
+
+class Board {
+public:
+    Board() {
+        construct(0, 0);
+    }
+
+    Board(int height, int width) {
+        construct(height, width);
+    }
+
+    void initalize() {
+        clear();
+        refresh();
+    }
+
+    void addBoarder() {
+        box(board_win, 0, 0);
+    }
+
+    void add(Drawable drawable) {
+        addAt(drawable.getY(), drawable.getX(), drawable.getIcon());
+    }
+
+    void addAt(int y, int x, chtype ch) {
+        mvwaddch(board_win, y, x, ch);
+    }
+
+    chtype getInput() {
+        return wgetch(board_win);
+    }
+
+    void clear() {
+        wclear(board_win);
+        addBoarder();
+    }
+
+    void refresh() {
+        wrefresh(board_win);
+    }
+
+private:
+    WINDOW *board_win;
+
+    void construct(int height, int width) {
+        int xMax, yMax;
+        getmaxyx(stdscr, yMax, xMax);
+
+        board_win = newwin(height, width, (yMax/2)-(height/2), (xMax/2)-(width/2));
+    }
+};
---
+++ view/Drawable.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+class Drawable {
+protected:
+    int y, x;
+    chtype icon;
+
+public:
+    Drawable() {
+        y = x = 0;
+        icon = ' ';
+    }
+
+    Drawable(int y, int x, chtype ch) {
+        this->y = y;
+        this->x = x;
+        this->icon = ch;
+    }
+
+    int getX() {return x;}
+
+    int getY() {return y;}
+
+    chtype getIcon() {return icon;}
+
+};
