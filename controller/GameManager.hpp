5fb71d924501bb99daa3b5a55a5765f56aebcf1d
always_spring01 <hangbum01@gmail.com>
2023-06-15 14:09:41 +0900

    modify controller - GameManager.hpp - growthitem 출력 구현

--- controller/GameManager.hpp
+++ controller/GameManager.hpp
@@ -1,31 +1,48 @@
-#include "../view/Board.hpp"
 #pragma once
 #include <ncurses.h>
+// for random
+#include <time.h>
+#include <stdlib.h>
+
+#include "../view/Board.hpp"
+#include "../view/Drawable.hpp"
+#include "../model/GrowthItem.hpp"
+#include "../model/Empty.hpp"
 
 class GameManager {
-private:
-  Board board;
-  bool game_over;
-
 public:
-  GameManager(int height, int width);
+    GameManager(int height, int width) {
+        board = Board(height, width);
+        board.initalize();
+        game_over = false;
+        srand(time(NULL));
+    }
 
-  void processInput() {
-    chtype input = board.getInput(); // wgetch();
-    // process input
-  }
+    ~GameManager() {delete growthitem;}
 
-  void updateState() {
-    // update state
-  }
+    void processInput() {
+        chtype input = board.getInput();
+        // process input
+    }
 
-  void redraw() { board.refresh(); }
-  bool isOver() { return game_over; }
+    void updateState() {
+        // update status
+        int y, x;
+        board.getEmptyCoordinates(y, x);
+        growthitem = new GrowthItem(y*2.5, x);
+        if (growthitem != NULL) board.add(Empty(growthitem->getY(), growthitem->getX()));
+        board.add(*growthitem);
+    }
+
+    void redraw() {
+        board.refresh();
+    }
+
+    bool isOver() {
+        return game_over;
+    }
+private:
+    Board board;
+    bool game_over;
+    GrowthItem *growthitem;
 };
-
-GameManager::GameManager(int height, int width) {
-  //
-  board = Board(height, width);
-  board.initalize();
-  game_over = false;
-}
